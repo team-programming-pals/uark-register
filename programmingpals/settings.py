0503a672c00cef4ccca7c544bf7879c94901d701
Michael <mhumland@uark.edu>
2020-08-31 12:03:52 -0500

    Initial commit

--- programmingpals/settings.py
+++ programmingpals/settings.py
@@ -121,7 +121,7 @@ STATIC_URL = '/static/'
 
 # Extra lookup directories for collectstatic to find static files
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'register/static'),
 )
 
 #  Add configuration for static files storage using whitenoise
