bce62db1dae7752b1f7cc94ed46cd1226bff0850
Michael <mhumland@uark.edu>
2020-08-31 11:56:40 -0500

    Initial commit

--- programmingpals/settings.py
+++ programmingpals/settings.py
@@ -9,9 +9,11 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
+
+import os
+import django_heroku
 import dj_database_url
 from pathlib import Path
-from os import path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
