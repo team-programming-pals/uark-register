95deb969a3f19657da25f439648f31af38c39b80
Michael <mhumland@uark.edu>
2020-08-31 11:52:35 -0500

    Initial Commit

--- programmingpals/settings.py
+++ programmingpals/settings.py
@@ -76,12 +76,7 @@ WSGI_APPLICATION = 'programmingpals.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        dj_database_url.config(default='postgres://localhost'),    
-    }
-}
-
+DATABASES = {'default': dj_database_url.config(default='postgres://localhost')}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
